17a58cc97ff4eefecc6fd7e343a36b8f52c20434
Lwandile Mfundisi <Lwandile.Mfundisi@takealot.com>
2026-02-02 21:26:47 +0200

    save

--- EventCatalogService.Api/Program.cs
+++ EventCatalogService.Api/Program.cs
@@ -1,46 +1,72 @@
-using EventCatalogService.Persistence;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
+using Duende.Bff;
+using EventCatalogService.Api.Extensions;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Serilog;
-using Serilog.Events;
-using Serilog.Sinks.SystemConsole.Themes;
-using XFrame.Persistence.EFCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System.Threading.Tasks;
+using EventCatalogService.Domain;
+using EventCatalogService.Persistence.Extensions;
 
-namespace EventCatalogService.Api
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSpaYarp();
+builder.Services.AddAuthorization();
+builder.Services.AddBff(o => o.ManagementBasePath = "/account")
+    .AddServerSideSessions();
+
+builder.Services.AddAuthentication(o =>
 {
-    public class Program
+    o.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    o.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+}).AddCookie(o =>
+{
+    o.Cookie.Name = "__Host-spa";
+    o.Cookie.SameSite = SameSiteMode.Strict;
+    o.Events.OnRedirectToLogin = (ctx) =>
     {
-        public static void Main(string[] args)
-        {
-            Log.Logger = new LoggerConfiguration()
-                .MinimumLevel.Debug()
-                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
-                .MinimumLevel.Override("Microsoft.Hosting.Lifetime", LogEventLevel.Information)
-                .MinimumLevel.Override("System", LogEventLevel.Warning)
-                .MinimumLevel.Override("Microsoft.AspNetCore.Authentication", LogEventLevel.Information)
-                .Enrich.FromLogContext()
-                .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}", theme: AnsiConsoleTheme.Code)
-                .CreateLogger();
+        ctx.Response.StatusCode = StatusCodes.Status401Unauthorized;
+        return Task.CompletedTask;
+    };
+})
+.AddOpenIdConnect(options =>
+{
+    options.Authority = "https://localhost:5001";
+    options.ClientId = "react";
+    options.ClientSecret = "49C1A7E1-0C79-4A89-A3D6-A37998FB86B0";
+    options.ResponseType = "code";
+});
 
-            var host = CreateHostBuilder(args).Build();
+builder.Services.AddLogging(l => l.AddConsole());
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "EventCatalogService.Api", Version = "v1" });
+});
 
-            using (var scope = host.Services.CreateScope())
-            {
-                var db = scope.ServiceProvider.GetRequiredService<IDbContextProvider<EventCatalogContext>>();
-                db.CreateContext().Database.Migrate();
-            }
+builder.Services
+    .AddControllers()
+    .AddNewtonsoftJson();
 
-            host.Run();
-        }
+builder.Services
+    .ConfigureApplicationInfrastructure();
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+builder.Services
+    .ConfigureEventCatalogServiceDomain()
+    .ConfigureEventCatalogPersistence();
 
-    }
-}
+var app = builder.Build();
+app.UseBff();
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+app.UseAuthorization();
+app.MapStaticAssets();
+app.MapControllers();
+app.MapBffManagementEndpoints();
+app.UseSpaYarp();
+
+
+app.Run();
